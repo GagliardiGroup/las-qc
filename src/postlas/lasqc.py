5c5de0816e791bd446d8dcf97310979d36fa2c2f
rdcunha <rdcunha@uchicago.edu>
2025-05-20 10:54:19 -0500

    create a LASQC wfn class

---
+++ src/postlas/lasqc.py
@@ -0,0 +1,45 @@
+#########################
+# Define LASQC class
+# Set up and run a LASSCF calculation
+# Other methods as solvers for LASQC
+#########################
+
+# PySCF imports
+from pyscf import gto, scf, lib
+# mrh imports
+from mrh.my_pyscf.mcscf.lasscf_o0 import LASSCF
+from mrh.my_pyscf.mcscf.lasci import h1e_for_cas
+
+from get_geom import get_geom
+
+# Define LASQC class
+class LASQC:
+    def __init__(self, mol, las=None, mf=None, frag_orbs=None, frag_elec=None, frag_atom_list=None, spin_sub=None):
+
+        if mf is None:
+            # Do RHF
+            mf = scf.RHF(mol).run()
+            print("HF energy: ", mf.e_tot)
+            
+        if las is None:
+            # Create LASSCF object
+            # Keywords: (wavefunction obj, num_orb in each subspace, (nelec in each subspace)/((num_alpha, num_beta) in each subspace), spin multiplicity in each subspace)
+            las = LASSCF(mf, frag_orbs, frag_elec, spin_sub=spin_sub)
+
+            # Localize the chosen fragment active spaces
+            loc_mo_coeff = las.localize_init_guess(frag_atom_list, mf.mo_coeff)
+
+            # Run LASSCF
+            las.kernel(loc_mo_coeff)
+            print("LASSCF energy: ", las.e_tot)
+
+        self.mf = mf
+        self.las = las
+
+if name == '__main__':
+
+    xyz = get_geom('scan', dist=args.dist)
+    mol = gto.M (atom = xyz, basis = 'sto-3g', output='h4_sto3g_{}.log'.format(args.dist),
+        symmetry=False, verbose=lib.logger.DEBUG)
+    lasqc_wfn = LASQC(mol)
+
