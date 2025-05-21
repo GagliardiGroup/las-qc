9ee9becc307cf23cca9f2780dc5c046eb027db49
Dustin Broderick <dustinr.broderick@gmail.com>
2025-05-21 16:00:46 -0500

    Adds default entry point

---
+++ las_qc/__main__.py
@@ -0,0 +1,19 @@
+from pyscf import gto, lib
+
+from .geometry import get_geom
+from .lasqc import LASQC
+
+
+def main():
+    """Helper function for running LAS-QC as a script
+    """
+    xyz = get_geom('far')
+    mol = gto.M (atom = xyz, basis = 'sto-3g', output='h4_sto3g.log',
+        symmetry=False, verbose=lib.logger.DEBUG)
+    lasqc_wfn = LASQC(mol, frag_orbs=(2,2), frag_elec=(2,2), frag_atom_list=((0,1),(2,3)), spin_sub=(1,1))
+
+
+if __name__ == '__main__':
+    # TODO: Parse commandline arguments
+    main()
+
