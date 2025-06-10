6b9b2dba15f690ad767318e30bf2bcc28105707e
Shreya Verma <vermashreya03@gmail.com>
2025-06-09 23:29:17 -0500

    edits in get_soci_vec

--- las_qc/lasucc.py
+++ las_qc/lasucc.py
@@ -39,33 +39,33 @@ class LASUCC:
         return excitations
 
     def get_soci_vec(ci_vec, num_spin_orbitals, nelec):
-    lookup_a = {}
-    lookup_b = {}
-    cnt = 0
-    norbs = num_spin_orbitals // 2
+        lookup_a = {}
+        lookup_b = {}
+        cnt = 0
+        norbs = num_spin_orbitals // 2
 
-    for ii in range(2**norbs):
-        if f"{ii:0{norbs}b}".count("1") == nelec[0]:
-            lookup_a[f"{ii:0{norbs}b}"] = cnt
-            cnt += 1
-    cnt = 0
-    for ii in range(2**norbs):
-        if f"{ii:0{norbs}b}".count("1") == nelec[1]:
-            lookup_b[f"{ii:0{norbs}b}"] = cnt
-            cnt += 1
+        for ii in range(2**norbs):
+            if f"{ii:0{norbs}b}".count("1") == nelec[0]:
+                lookup_a[f"{ii:0{norbs}b}"] = cnt
+                cnt += 1
+        cnt = 0
+        for ii in range(2**norbs):
+            if f"{ii:0{norbs}b}".count("1") == nelec[1]:
+                lookup_b[f"{ii:0{norbs}b}"] = cnt
+                cnt += 1
 
-    soci_vec = np.zeros(2**num_spin_orbitals)
-    for kk in range(2**num_spin_orbitals):
-        if (
-            f"{kk:0{num_spin_orbitals}b}"[norbs:].count("1") == nelec[0]
-            and f"{kk:0{num_spin_orbitals}b}"[:norbs].count("1") == nelec[1]
-        ):
-            so_ci_vec[kk] = ci_vec[
-                lookup_a[f"{kk:0{num_spin_orbitals}b}"[norbs:]],
-                lookup_b[f"{kk:0{num_spin_orbitals}b}"[:norbs]],
-            ]
+        soci_vec = np.zeros(2**num_spin_orbitals)
+        for kk in range(2**num_spin_orbitals):
+            if (
+                f"{kk:0{num_spin_orbitals}b}"[norbs:].count("1") == nelec[0]
+                and f"{kk:0{num_spin_orbitals}b}"[:norbs].count("1") == nelec[1]
+            ):
+                so_ci_vec[kk] = ci_vec[
+                    lookup_a[f"{kk:0{num_spin_orbitals}b}"[norbs:]],
+                    lookup_b[f"{kk:0{num_spin_orbitals}b}"[:norbs]],
+                ]
 
-    return soci_vec
+        return soci_vec
 
 
     def initialize_fragments(self,mapper=JordanwignerMapper(), estimator=None, optimizer, optimize=True):
