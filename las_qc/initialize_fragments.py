63ee1cf372f44dd7675e8acffde5da2ef39d7646
Shreya Verma <vermashreya03@gmail.com>
2025-06-12 22:53:59 -0500

    added the DI

--- las_qc/initialize_fragments.py
+++ las_qc/initialize_fragments.py
@@ -1,4 +1,5 @@
 import numpy as np
+from qiskit import QuantumRegister, QuantumCircuit
 
 '''
 This is for returning a quantum circuit. 
@@ -9,12 +10,12 @@ Four methods of initializing fragments:
     4. VQE
 '''
 
-def get_soci_vec(ci_vec, num_spin_orbitals, nelec):
+def get_soci_vec(ci_vec, nso, nelec):
         '''This is for DI'''
         lookup_a = {}
         lookup_b = {}
         cnt = 0
-        norbs = num_spin_orbitals // 2
+        norbs = nso // 2
 
         for ii in range(2**norbs):
             if f"{ii:0{norbs}b}".count("1") == nelec[0]:
@@ -26,21 +27,37 @@ def get_soci_vec(ci_vec, num_spin_orbitals, nelec):
                 lookup_b[f"{ii:0{norbs}b}"] = cnt
                 cnt += 1
 
-        soci_vec = np.zeros(2**num_spin_orbitals)
-        for kk in range(2**num_spin_orbitals):
+        soci_vec = np.zeros(2**nso)
+        for kk in range(2**nso):
             if (
-                f"{kk:0{num_spin_orbitals}b}"[norbs:].count("1") == nelec[0]
-                and f"{kk:0{num_spin_orbitals}b}"[:norbs].count("1") == nelec[1]
+                f"{kk:0{nso}b}"[norbs:].count("1") == nelec[0]
+                and f"{kk:0{nso}b}"[:norbs].count("1") == nelec[1]
             ):
                 so_ci_vec[kk] = ci_vec[
-                    lookup_a[f"{kk:0{num_spin_orbitals}b}"[norbs:]],
-                    lookup_b[f"{kk:0{num_spin_orbitals}b}"[:norbs]],
+                    lookup_a[f"{kk:0{nso}b}"[norbs:]],
+                    lookup_b[f"{kk:0{nso}b}"[:norbs]],
                 ]
 
         return soci_vec
 
-def direct_initialization():
-    ...
+def direct_initialization(las):
+    ncas = np.sum(las.ncas_sub)
+    nqubits = 2*ncas
+    qubits = np.arange(nqubits).tolist()
+    frag_qubits = []
+    i = 0
+    for f in las.ncas_sub:
+        frag_qubits.append(qubits[i : i + f] + qubits[ncas + i : ncas + i + f])
+        i += f
+
+    qr = QuantumRegister(nqubits) #SV (nqubits, 'lasq'); not sure if a name is needed
+    circuit = QuantumCircuit(qr)
+
+    for frag in range(len(las.ncas_sub)):
+            circuit.initialize(get_soci_vec(las.ci[frag][0], 2*las.ncas_sub[frag], las.nelecas_sub[frag]), frag_qubits[frag])
+
+    return circuit
+
 
 def spectral_filtering():
     ...
