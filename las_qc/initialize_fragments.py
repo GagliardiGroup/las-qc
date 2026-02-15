ca2dfe5d8f632a58e25f143e2fd78264bbaedf66
Shreya Verma <vermashreya03@gmail.com>
2026-02-14 21:59:37 -0600

    File for initializing fragments, just DI for now

--- las_qc/initialize_fragments.py
+++ las_qc/initialize_fragments.py
@@ -28,18 +28,16 @@ def get_soci_vec(ci_vec, nso, nelec):
             lookup_b[f"{ii:0{norbs}b}"] = cnt
             cnt += 1
 
-    soci_vec = np.zeros(2**nso)
-    for kk in range(2**nso):
-        if (
-            f"{kk:0{nso}b}"[norbs:].count("1") == nelec[0]
-            and f"{kk:0{nso}b}"[:norbs].count("1") == nelec[1]
-        ):
-            so_ci_vec[kk] = ci_vec[
-                lookup_a[f"{kk:0{nso}b}"[norbs:]],
-                lookup_b[f"{kk:0{nso}b}"[:norbs]],
-            ]
-
-    return soci_vec
+        soci_vec = np.zeros(2**nso)
+        for kk in range(2**nso):
+            if (
+                f"{kk:0{nso}b}"[norbs:].count("1") == nelec[0]
+                and f"{kk:0{nso}b}"[:norbs].count("1") == nelec[1]
+            ):
+                soci_vec[kk] = ci_vec[
+                    lookup_a[f"{kk:0{nso}b}"[norbs:]],
+                    lookup_b[f"{kk:0{nso}b}"[:norbs]],
+                ]
 
 
 def direct_initialization(las):
